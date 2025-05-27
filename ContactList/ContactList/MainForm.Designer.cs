9be7383dcc26b0550bea90859fe2ef2387e5a7f5
Trung0btCode <cutrung2k4@gmail.com>
2025-05-27 09:00:42 +0700

    Thêm Form để thêm liên hệ mới

--- ContactList/ContactList/MainForm.Designer.cs
+++ ContactList/ContactList/MainForm.Designer.cs
@@ -36,28 +36,31 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(271, 26);
+            this.label1.Location = new System.Drawing.Point(241, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(266, 29);
+            this.label1.Size = new System.Drawing.Size(227, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "DANH SÁCH LIÊN HỆ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(637, 399);
+            this.button1.Location = new System.Drawing.Point(566, 319);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 39);
+            this.button1.Size = new System.Drawing.Size(134, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Thêm liên hệ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
